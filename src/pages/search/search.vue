671b100dec90c3f34dc0f1d5295f3cb7de43875d
xjw <xuejiawei@huntersay.com>
2019-10-24 18:40:51 +0800

    '完成TagGroup组件开发'

--- src/pages/search/search.vue
+++ src/pages/search/search.vue
@@ -1,18 +1,37 @@
 <template>
   <div>
-    <Tag text="测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本测试文本" @onClick="onTagClick"/>
+    <TagGroup
+      header-text="热门搜索"
+      btn-text="换一批"
+      :value="value"
+      @onBtnClick="changeHotSearch"
+      @onTagClick="showBookDetail"
+    />
   </div>
 </template>
 
 <script>
 import Tag from '@/components/base/Tag'
+import TagGroup from '@/components/base/TagGroup'
 export default {
   components: {
-    Tag
+    Tag,
+    TagGroup
+  },
+  data () {
+    return {
+      value: ['111', '222', '333', 'dfdsfsdfsdfffffffffffffffff']
+    }
   },
   methods: {
     onTagClick () {
       console.log('tag')
+    },
+    showBookDetail (text, index) {
+      console.log(text, index)
+    },
+    changeHotSearch () {
+      console.log('换一批')
     }
   }
 }
