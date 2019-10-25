09ffa0e988d95992294fe608a6ba1b3e81c33fc9
xjw <xuejiawei@huntersay.com>
2019-10-25 11:33:25 +0800

    '搜索模块组件集成'

--- src/pages/search/search.vue
+++ src/pages/search/search.vue
@@ -1,18 +1,35 @@
 <template>
   <div>
+    <SearchBar 
+      :focus="searchFocus"
+      @onChange="onChange"
+    />
     <TagGroup
       header-text="热门搜索"
       btn-text="换一批"
-      :value="value"
+      :value="hotSearch"
       @onBtnClick="changeHotSearch"
       @onTagClick="showBookDetail"
+      v-if="hotSearch.length > 0 && !showList"
     />
-    <SearchList :data="searchList"/>
+    <TagGroup
+      header-text="历史搜索"
+      btn-text="清空"
+      :value="historySearch"
+      @onBtnClick="clearHistorySearch"
+      @onTagClick="searchkeyWord"
+      v-if="historySearch.length > 0 && !showList"
+    />
+    <SearchList 
+      :data="searchList"
+      v-if="showList"
+    />
   </div>
 </template>
 
 <script>
 /* eslint-disable */
+import SearchBar from '@/components/home/SearchBar'
 import Tag from '@/components/base/Tag'
 import TagGroup from '@/components/base/TagGroup'
 import SearchItem from '@/components/search/SearchItem'
@@ -20,17 +37,33 @@ import SearchTable from '@/components/search/SearchTable'
 import SearchList from '@/components/search/SearchList'
 export default {
   components: {
+    SearchBar,
     Tag,
     TagGroup,
     SearchItem,
-    SearchTable
+    SearchTable,
+    SearchList
+  },
+  computed: {
+    showList () {
+      const keys = Object.keys(this.searchList)
+      return keys.length > 0
+    }
   },
   data () {
     return {
-      searchList: {}
+      searchList: {},
+      hotSearch: [],
+      historySearch: [],
+      searchFocus: true
     }
   },
   methods: {
+    onChange (keyword) {
+      console.log(keyword)
+    },
+    clearHistorySearch () {},
+    searchkeyWord () {},
     onTagClick () {
       console.log('tag')
     },
