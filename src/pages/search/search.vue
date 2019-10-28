883b25eb562dd7e12dd4bd72181dda58b5953a77
coder_xue <857986029@qq.com>
2019-10-28 14:39:21 +0800

    '完成历史搜索和热搜更新功能'

--- src/pages/search/search.vue
+++ src/pages/search/search.vue
@@ -6,6 +6,7 @@
       @onChange="onChange"
       @onClear="onClear"
       @onConfirm="onConfirm"
+      ref="searchBar"
     />
     <TagGroup
       header-text="热门搜索"
@@ -39,7 +40,7 @@ import SearchItem from '@/components/search/SearchItem'
 import SearchTable from '@/components/search/SearchTable'
 import SearchList from '@/components/search/SearchList'
 import { search, hotSearch } from '@/api'
-import { getStorageSync } from '@/api/wechat'
+import { getStorageSync, setStorageSync } from '@/api/wechat'
 export default {
   components: {
     SearchBar,
@@ -75,13 +76,18 @@ export default {
       // 1.判断是否有搜索关键词，如果没有，则获取热门搜索词，通过热门搜索词发起请求，如果有，就用搜索关键词发起请求
       if (!keyword || keyword.trim().length === 0) {
         keyword = this.hotSearchKeyword
+        this.$refs.searchBar.setValue(keyword)
       } else {
 
       }
       this.onSearch(keyword)
       // 2.将搜索结果写入历史搜索
-      // 3.将搜索框市区焦点
-      console.log(keyword)
+      if(!this.historySearch.includes(keyword)) {
+        this.historySearch.push(keyword)
+        setStorageSync('historySearch', this.historySearch)
+      }
+      // 3.将搜索框失去焦点
+      this.searchFocus = false
     },
     onClear () {
       this.searchList = {}
@@ -101,8 +107,14 @@ export default {
         this.searchList = res.data.data
       })
     },
-    clearHistorySearch () {},
-    searchkeyWord () {},
+    clearHistorySearch () {
+      this.historySearch = []
+      setStorageSync('historySearch', [])
+    },
+    searchkeyWord (text) {
+      this.$refs.searchBar.setValue(text)
+      this.onSearch(text)
+    },
     onTagClick () {
       console.log('tag')
     },
@@ -110,7 +122,9 @@ export default {
       console.log(text, index)
     },
     changeHotSearch () {
-      console.log('换一批')
+      hotSearch().then(res => {
+        this.hotSearch = res.data.data
+      })
     }
   },
   mounted () {
@@ -119,6 +133,7 @@ export default {
       this.hotSearch = res.data.data
     })
     this.hotSearchKeyword = this.$route.query.hotSearch
+    this.historySearch = getStorageSync('historySearch') || []
   }
 }
 </script>
