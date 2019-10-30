f1073797c802f94b80e6bcf499b3a0aeb9218106
xjw <xuejiawei@huntersay.com>
2019-10-30 14:18:21 +0800

    '实现加入和移出书架功能'

--- src/api/index.js
+++ src/api/index.js
@@ -55,3 +55,19 @@ export function bookRankSave (params) {
 export function bookContents (params) {
   return get(`${API_URL}/book/contents`, params)
 }
+
+export function bookIsInShelf (params) {
+  return get(`${API_URL}/book/shelf/get`, params)
+}
+
+export function bookShelfSave (params) {
+  return get(`${API_URL}/book/shelf/save`, {
+    shelf: JSON.stringify(params)
+  })
+}
+
+export function bookShelfRemove (params) {
+  return get(`${API_URL}/book/shelf/remove`, {
+    shelf: JSON.stringify(params)
+  })
+}
--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -15,6 +15,30 @@
       :contents="contents"
       @readBook="readBook"
     />
+    <!-- <DetailBottom 
+      :isInShelf="isInShelf"
+      @handleShelf="handleShelf"
+    /> -->
+    <div class="detail-bottom">
+      <div class="detail-btn-wrapper">
+        <van-button
+          :custom-class="isInShelf ? 'detail-btn-remove' : 'detail-btn-shelf'"
+          round
+          @click="handleShelf"
+        >
+          {{isInShelf ? '移出书架' : '加入书架'}}
+        </van-button>
+      </div>
+      <div class="detail-btn-wrapper">
+        <van-button
+          custom-class="detail-btn-read"
+          round
+          @click="() => readBook()"
+        >
+          阅读
+        </van-button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -23,19 +47,22 @@ import DetailBook from '@/components/detail/DetailBook'
 import DetailStat from '@/components/detail/DetailStat'
 import DetailRate from '@/components/detail/DetailRate'
 import DetailContents from '@/components/detail/DetailContents'
-import { bookDetail, bookRankSave, bookContents } from '@/api'
+import DetailBottom from '@/components/detail/DetailBottom'
+import { bookDetail, bookRankSave, bookContents, bookIsInShelf, bookShelfSave, bookShelfRemove } from '@/api'
 import { getStorageSync } from '@/api/wechat'
 export default {
   components: {
     DetailBook,
     DetailStat,
     DetailRate,
-    DetailContents
+    DetailContents,
+    DetailBottom
   },
   data () {
     return {
       book: {},
-      contents: []
+      contents: [],
+      isInShelf: false
     }
   },
   methods: {
@@ -63,21 +90,102 @@ export default {
       const { fileName } = this.$route.query
       if (fileName) {
         bookContents({fileName}).then(res => {
+          console.log('目录', res.data.data)
           this.contents = res.data.data
         })
       }
     },
     readBook (href) {
 
+    },
+    getBookIsInShelf () {
+      const openId = getStorageSync('openId')
+      const { fileName } = this.$route.query
+      if (openId && fileName) {
+        bookIsInShelf({ openId, fileName }).then(res => {
+          const { data } = res.data
+          if (data.length === 0) {
+            this.isInShelf = false
+          } else {
+            this.isInShelf = true
+          }
+        })
+      }
+    },
+    // 加入-移出书架
+    handleShelf () {
+      const openId = getStorageSync('openId')
+      const { fileName } = this.$route.query
+      if (!this.isInShelf) {
+        bookShelfSave({ openId, fileName }).then(res => {
+          this.getBookIsInShelf()
+        })
+      } else {
+        const vue = this
+        mpvue.showModal({
+          title: '提示',
+          content: `是否确认将《${this.book.title}》移出书架?`,
+          success (res) {
+            if (res.confirm) {
+              bookShelfRemove({ openId, fileName }).then(res => {
+                vue.getBookIsInShelf()
+              })
+            } else {
+              console.log('用户点击取消')
+            }
+          }
+        })
+      }
     }
   },
   mounted () {
     this.getBookDetail()
     this.getBookContents()
+    this.getBookIsInShelf()
   }
 }
 </script>
 
 <style lang="scss" scoped>
+  .detail-bottom {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    background: #fff;
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    padding: 0 15px;
+    border-top: 1px solid #eee;
+    box-shadow: 0 -2px 4px 0 rgba(0,0,0,.1);
 
+    .detail-btn-wrapper {
+      flex: 1;
+
+      .detail-btn-read {
+        width: 100%;
+        border: none;
+        color: #fff;
+        background: #1EA3F5;
+        margin-left: 7.5px;
+      }
+
+      .detail-btn-shelf {
+        width: 100%;
+        color: #1EA3F5;
+        background: #fff;
+        border: 1px solid #1EA3F5;
+        margin-right: 7.5px;
+      }
+
+      .detail-btn-remove {
+        width: 100%;
+        color: #F96128;
+        background: rgba(255, 175, 155, .3);
+        border: 1px solid #FFAF9B;
+        margin-right: 7.5px;
+      }
+    }
+  }
 </style>
